1639351481cff7ede3f0992348386e2215db2e4b
Piyush K. Das <piyush.k.das25@gmail.com>
2023-07-09 13:32:34 +0530

    added files from device

---
+++ script.js
@@ -0,0 +1,17 @@
+let shareButtons = document.querySelectorAll(".tile-share-button")
+console.log(shareButtons)
+
+async function copyText(e) {
+    e.preventDefault();
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text " + link)
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+shareButtons.forEach(shareButton => shareButton.addEventListener("click", copyText))
